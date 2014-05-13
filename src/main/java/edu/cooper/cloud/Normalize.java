85cd92be8c62191b1ecfabe2a73117cd2b62aca7
Sameer <schauhan19@gmail.com>
2014-05-13 17:40:51 -0400

    Mapper Kinda Works

--- src/main/java/edu/cooper/cloud/Normalize.java
+++ src/main/java/edu/cooper/cloud/Normalize.java
@@ -19,7 +19,6 @@ package edu.cooper.cloud;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
@@ -28,26 +27,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Normalize {
 
-    public class DoubleArrayWritable extends ArrayWritable {
 
-        public DoubleArrayWritable() {
-            super(DoubleWritable.class);
-        }
 
-        public DoubleArrayWritable(DoubleWritable[] values) {
-            super(DoubleWritable.class, values);
-        }
-
-        public DoubleWritable get(int idx) {
-            return (DoubleWritable) get()[idx];
-        }
-    }
-
-    public class NormalizeMapper extends Mapper <LongWritable, DoubleArrayWritable, IntWritable,DoubleArrayWritable> {
+    public static class NormalizeMapper extends Mapper <LongWritable, DoubleArrayWritable, IntWritable,DoubleArrayWritable> {
 
         private int dataDimFrom;
         private int dataDimTo;
@@ -82,7 +67,7 @@ public class Normalize {
 
     }
 
-    public class NormalizeReducer extends Reducer <IntWritable, DoubleArrayWritable, IntWritable, DoubleArrayWritable> {
+    public static class NormalizeReducer extends Reducer <IntWritable, DoubleArrayWritable, IntWritable, DoubleArrayWritable> {
 
         private int dataDimFrom;
         private int dataDimTo;
@@ -125,7 +110,7 @@ public class Normalize {
 
 
 
-    public class NormalizeCombiner extends Reducer <IntWritable, DoubleArrayWritable, IntWritable, DoubleArrayWritable> {
+    public static class NormalizeCombiner extends Reducer <IntWritable, DoubleArrayWritable, IntWritable, DoubleArrayWritable> {
 
         private int dataDimFrom;
         private int dataDimTo;
@@ -165,8 +150,8 @@ public class Normalize {
 
     public static void main(String[] args) throws Exception {
 
-        String input = "/datasets/train_subject01.csv";
-        String output= "/output/trainX.csv";
+        String input = "datasets/train_subject01.csv";
+        String output= "output/trainX2.csv";
 
         Configuration conf = new Configuration();
         Map<String, String> env = System.getenv();
