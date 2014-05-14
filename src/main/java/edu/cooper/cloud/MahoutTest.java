76ae728d2487c402d0d3cc34dc523206e8dc4950
Sameer <schauhan19@gmail.com>
2014-05-13 21:45:21 -0400

    Attempting to use new mahout.

--- src/main/java/edu/cooper/cloud/MahoutTest.java
+++ src/main/java/edu/cooper/cloud/MahoutTest.java
@@ -10,22 +10,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.classifier.bayes.algorithm.BayesAlgorithm;
-import org.apache.mahout.classifier.bayes.common.BayesParameters;
-import org.apache.mahout.classifier.bayes.datastore.InMemoryBayesDatastore;
-import org.apache.mahout.classifier.bayes.exceptions.InvalidDatastoreException;
-import org.apache.mahout.classifier.bayes.interfaces.Algorithm;
-import org.apache.mahout.classifier.bayes.interfaces.Datastore;
-import org.apache.mahout.classifier.bayes.model.ClassifierContext;
-import org.apache.mahout.math.NamedVector;
-import org.apache.mahout.math.VectorWritable;
-import org.apache.mahout.utils.vectors.csv.CSVVectorIterator;
-import org.apache.mahout.classifier.bayes.*;
+import org.apache.mahout.classifier.AbstractVectorClassifier;
+import org.apache.mahout.classifier.ClassifierResult;
+import org.apache.mahout.classifier.naivebayes.*;
+import org.apache.mahout.classifier.naivebayes.training.TrainNaiveBayesJob;
 
 
 public class MahoutTest {
 
-    public static void main(String[] args) throws IOException, TasteException, InvalidDatastoreException {
+    public static void main(String[] args) throws IOException, TasteException {
 
         System.out.println("Hello World ");
         System.out.println("Training");
@@ -33,49 +26,73 @@ public class MahoutTest {
             System.out.println(word);
         }
 
-        String input = "datasets/data.dat";
-        String output= "output/outp.out";
+        String input = "datasets/iris.data";
+        String output= "output/out.out";
 
         FileSystem fs = null;
         SequenceFile.Writer writer;
         Configuration conf = new Configuration();
-        BufferedReader reader = new BufferedReader(new FileReader("datasets/data.dat"));
+        TrainNaiveBayesJob trainNaiveBayes = new TrainNaiveBayesJob();
+        trainNaiveBayes.setConf(conf);
+//        trainNaiveBayes.run()l
+//        https://svn.apache.org/repos/asf/mahout/tags/mahout-0.7/core/src/test/java/org/apache/mahout/classifier/naivebayes/NaiveBayesTest.java
+        NaiveBayesModel nbMobel = NaiveBayesModel.fromMRTrainerOutput(new Path(output), conf);
+        AbstractVectorClassifier classifier = new StandardNaiveBayesClassifier(nbMobel);
 
-        CSVVectorIterator it = new CSVVectorIterator(reader);
-        List<NamedVector> vectors = new ArrayList<NamedVector>();
-        while(it.hasNext()){
-            vectors.add(new NamedVector(it.next(),"Test"));
-        }
 
-        fs = FileSystem.get(conf);
-        Path path = new Path("output/out.seq");
-        writer = new SequenceFile.Writer(fs, conf, path, Text.class, Text.class);
 
-        VectorWritable vec = new VectorWritable();
-        for (NamedVector vector : vectors) {
-            vec.set(vector);
-            writer.append(new Text(vector.getName()), vec);
-        }
-        writer.close();
+//        BufferedReader reader = new BufferedReader(new FileReader(output));
 
-        BayesParameters bayesParameters = new BayesParameters();
-        bayesParameters.setGramSize( 1 );
-        bayesParameters.set( "verbose", "true" );
-        bayesParameters.set( "classifierType", "bayes" );
-        bayesParameters.set( "defaultCat", "OTHER" );
-        bayesParameters.set( "encoding", "UTF-8" );
-        bayesParameters.set( "alpha_i", "1.0" );
-        bayesParameters.set( "dataSource", "hdfs" );
-        bayesParameters.set("basePath", output);
+//        CSVVectorIterator it = new CSVVectorIterator(reader);
+//        List<NamedVector> vectors = new ArrayList<NamedVector>();
+//        while(it.hasNext()){
+//            vectors.add(new NamedVector(it.next(),"Test"));
+//        }
+//
+//        fs = FileSystem.get(conf);
+//        Path path = new Path("output/out.seq");
+//        writer = new SequenceFile.Writer(fs, conf, path, Text.class, VectorWritable.class);
+//
+//        VectorWritable vec = new VectorWritable();
+//        for (NamedVector vector : vectors) {
+//            vec.set(vector);
+//            writer.append(new Text(vector.getName()), vec);
+//        }
+//        writer.close();
 
-//        Train the parameters
-        TrainClassifier.trainNaiveBayes(new Path(input), new Path(output), bayesParameters);
+//        BayesParameters bayesParameters = new BayesParameters();
+//        bayesParameters.setGramSize( 1 );
+//        bayesParameters.set("verbose", "true");
+//        bayesParameters.set( "classifierType", "bayes" );
+//        bayesParameters.set( "defaultCat", "OTHER" );
+//        bayesParameters.set( "encoding", "UTF-8" );
+//        bayesParameters.set( "alpha_i", "1.0" );
+//        bayesParameters.set( "dataSource", "hdfs" );
+//        bayesParameters.set("basePath", output);
+//
+////        Train the parameters
+//        TrainClassifier.trainNaiveBayes(new Path(input), new Path(output), bayesParameters);
 
 //        Make Classifier
-        Algorithm algorithm = new BayesAlgorithm();
-        Datastore datastore = new InMemoryBayesDatastore( bayesParameters );
-        ClassifierContext classifier = new ClassifierContext( algorithm, datastore );
-        classifier.initialize();
+//        Algorithm algorithm = new BayesAlgorithm();
+//        Datastore datastore = new InMemoryBayesDatastore( bayesParameters );
+//        ClassifierContext classifier = new ClassifierContext( algorithm, datastore );
+//        classifier.initialize();
+//
+//        BufferedReader reader = new BufferedReader( new FileReader( input ) );
+//        String entry = reader.readLine();
+//         while( entry != null ) {
+//            List<String> document = Arrays.asList(entry.split("\\s*,\\s*"));
+////            List< String > document = new NGrams( entry,
+////                    Integer.parseInt( bayesParameters.get( "gramSize" ) ) )
+////                    .generateNGramsWithoutLabel();
+//
+//            ClassifierResult result =
+//                    classifier.classifyDocument( document.toArray( new String[document.size()]),
+//                            bayesParameters.get( "defaultCat" ) );
+//            entry = reader.readLine();
+//            System.out.println(result.getLabel()+"  " +result.getScore());
+//        }
 
 
 
